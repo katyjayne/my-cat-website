eff1c274ba6c5d022e399206b68bddc4cf691c1b
katyjayne <katyjayneblank@gmail.com>
2024-02-22 13:04:27 -0500

    Added SelectedBreed functions and additional css

--- src/App.jsx
+++ src/App.jsx
@@ -10,7 +10,7 @@ function App() {
 
   useEffect(() => {
     async function getRandomImage() {
-      const response = await fetch('https://api.thecatapi.com/v1/images/search')
+      const response = await fetch(fetchUrl)
       const result = await response.json()
       setCatImgUrl(result[0].url)
     }
@@ -18,15 +18,27 @@ function App() {
   }, [])
 
   const updateImage = async() => {
-    const response = await fetch('https://api.thecatapi.com/v1/images/search')
+    const response = await fetch(fetchUrl)
     const result = await response.json()
     setCatImgUrl(result[0].url)
   }
+
+  console.log(selectedBreed)
+
+  const API_URL = `https://api.thecatapi.com/v1/images/search`
+  const queryString = `?breed_ids=${selectedBreed}`
+
+  const fetchUrl = selectedBreed === ''
+    ? API_URL
+    : API_URL + queryString
+
+    console.log(fetchUrl)
 
   return (
     <>
       {/* make an input that selects a specific cat breed with hard-coded options*/}
       <SelectBreed selectedBreed={selectedBreed} setSelectedBreed={setSelectedBreed} />
+      <br></br>
       {/* record the cat breed in the App component state */}
       {/* if we are searching for a specific breed, apply that criteria to the fetch request */}
       {/* then, fetch the breeds list and update the input to use the list of available breeds */}
--- src/components/SelectBreed.jsx
+++ src/components/SelectBreed.jsx
@@ -1,17 +1,33 @@
-import { FormControl, InputLabel, Select } from "@mui/material"
+import { FormControl, InputLabel, MenuItem, Select } from "@mui/material"
+import { useEffect, useState } from "react"
 
 
 function SelectBreed({selectedBreed, setSelectedBreed}) {
+  const [breedList, setBreedList] = useState([])
+
+  useEffect(() => {
+    async function getBreedList() {
+      const response = await fetch('https://api.thecatapi.com/v1/breeds')
+      const result = await response.json()
+      setBreedList(result)
+    }
+    getBreedList()
+  }, [])
+
   return (
-    <FormControl>
+    <FormControl fullWidth>
       <InputLabel id="breed-select-label">Breed</InputLabel>
       <Select
         labelId="breed-select-label"
         id="breed-select"
         value={selectedBreed}
-        label=""
+        label="Breed"
         onChange={(e) => setSelectedBreed(e.target.value)}
-        />
+        >
+          {breedList.map(breed => <MenuItem value={breed.id}>{breed.name}</MenuItem>)}
+          {/* <MenuItem value="munc">Munchkin</MenuItem>
+          <MenuItem value="asho">American Shorthair</MenuItem> */}
+        </Select>
     </FormControl>
   )
 }
